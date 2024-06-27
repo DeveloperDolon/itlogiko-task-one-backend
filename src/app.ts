5473c204f2869e5009c495b9e1857d7c64442815
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-27 19:24:18 +0600

    project connect with database setup commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { Application, Request, Response } from 'express';
+import express from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to IT LOGIKO Server...!');
+});
+
+export default app;
