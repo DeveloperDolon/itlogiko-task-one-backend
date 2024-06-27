d0e8586fa8b8b31feb97b64fb350d03bc4e62763
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-27 20:07:20 +0600

    application data model added commit

---
+++ src/app/modules/application/application.interface.ts
@@ -0,0 +1,7 @@
+export interface TApplication {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  cv: File;
+}
---
+++ src/app/modules/application/application.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { TApplication } from './application.interface';
+
+const applicationSchema = new Schema<TApplication>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    cv: {
+      type: {
+        filename: String,
+        path: String,
+        mimeType: String,
+        size: Number,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ApplicationModel = model<TApplication>(
+  'Application',
+  applicationSchema,
+);
