675a94cba36d74f062791f7bf92a132b0b40f565
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-28 15:58:12 +0600

    user schema added commit

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    required: true,
+  },
+});
+
+export const UserModel = model<TUser>('User', userSchema);
